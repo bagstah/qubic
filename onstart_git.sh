0df1b3b78942fe71641f6b7ab54173f77f0a73bc
bagstah <pelletiy@gmail.com>
2024-05-13 13:41:44 -0400

    Update onstart_git.sh

--- onstart_git.sh
+++ onstart_git.sh
@@ -5,8 +5,8 @@ wget https://raw.githubusercontent.com/bagstah/qubic/main/appsettings.json
 sed -i "s/vastai1/$CONTAINER_ID/g" appsettings.json
 wget https://dl.qubic.li/downloads/qli-Client-1.9.6-Linux-x64.tar.gz
 tar zxvf qli-Client-1.9.6-Linux-x64.tar.gz qli-Client
-apt install screen
-apt install vim
+apt -y install screen vim
+#apt -y install vim
 mkdir cpu
 cat appsettings.json | grep -v trainer > cpu/appsettings.json
 cp qli-Client cpu

6a60b1be3637d5e82e893e137d716f72d3001ce2
bagstah <pelletiy@gmail.com>
2024-05-15 16:25:16 -0400

    Update onstart_git.sh

--- onstart_git.sh
+++ onstart_git.sh
@@ -6,12 +6,17 @@ sed -i "s/vastai1/$CONTAINER_ID/g" appsettings.json
 wget https://dl.qubic.li/downloads/qli-Client-1.9.6-Linux-x64.tar.gz
 tar zxvf qli-Client-1.9.6-Linux-x64.tar.gz qli-Client
 apt -y install screen vim
-#apt -y install vim
 mkdir cpu
-cat appsettings.json | grep -v trainer > cpu/appsettings.json
+
+#NEW STUFF
+myproc=`nproc`
+cat appsettings.json | grep -v trainer | grep -v thread > appsettings.GPU
+cat appsettings.GPU | sed -e '5i\ \ \ \ "amountOfThreads": "10",' > cpu/appsettings.json
+#END NEW STUFF
+
 cp qli-Client cpu
 rm qli-runner*
 rm *.lock
-wget https://github.com/Gddrig/Qubic_Hiveos/releases/download/0.5.0/rqiner-x86-cuda-Nvidia.0.5.0.tar.gz
-tar zxvf rqiner-x86-cuda-Nvidia.0.5.0.tar.gz
+#wget https://github.com/Gddrig/Qubic_Hiveos/releases/download/0.5.0/rqiner-x86-cuda-Nvidia.0.5.0.tar.gz
+#tar zxvf rqiner-x86-cuda-Nvidia.0.5.0.tar.gz
 screen -S gpu -d -m ./qli-Client

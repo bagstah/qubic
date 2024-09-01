51de63c4beaca4ee3d44f8f9ef10d77683208774
bagstah <pelletiy@gmail.com>
2024-08-31 23:17:23 -0400

    Update onstart_evr.sh

--- onstart_evr.sh
+++ onstart_evr.sh
@@ -1,10 +1,5 @@
-env >> /etc/environment;
-
-apt -y install screen vim
-sleep 10
-myproc=`expr \`nproc\` - 2`
-
+rm nanominer*
 wget https://github.com/nanopool/nanominer/releases/download/v3.9.2/nanominer-linux-3.9.2.tar.gz
 tar zxvf nanominer-linux-3.9.2.tar.gz nanominer
 chmod +x nanominer
-screen -S gpu -d -m ./nanominer -algo Evrprogpow -wallet EPNfS782KjhfGeY162ezHiYff3my2CHCuf -pool1 us.evrpool.org:1111
+screen -S gpu -d -m ./nanominer -algo Evrprogpow -wallet EPNfS782KjhfGeY162ezHiYff3my2CHCuf -rigName $CONTAINER_ID -pool1 us.evrpool.org:1111

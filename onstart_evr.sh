23995d21a687cc3ae7992871180bc05ad62c4261
bagstah <pelletiy@gmail.com>
2024-08-31 22:47:53 -0400

    Update onstart_evr.sh

--- onstart_evr.sh
+++ onstart_evr.sh
@@ -7,4 +7,4 @@ myproc=`expr \`nproc\` - 2`
 wget https://github.com/nanopool/nanominer/releases/download/v3.9.2/nanominer-linux-3.9.2.tar.gz
 tar zxvf nanominer-linux-3.9.2.tar.gz nanominer
 chmod +x nanominer
-screen -S gpu -d -m ./nanominer -algo Evrprogpow -wallet EPNfS782KjhfGeY162ezHiYff3my2CHCuf -pool1 us.evrpool.com:1111
+screen -S gpu -d -m ./nanominer -algo Evrprogpow -wallet EPNfS782KjhfGeY162ezHiYff3my2CHCuf -pool1 us.evrpool.org:1111

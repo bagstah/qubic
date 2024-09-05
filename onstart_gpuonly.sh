f84d24888795cca69931d79273764060383f8059
bagstah <pelletiy@gmail.com>
2024-09-04 23:38:25 -0400

    Update onstart_gpuonly.sh

--- onstart_gpuonly.sh
+++ onstart_gpuonly.sh
@@ -4,8 +4,8 @@ rm -rf cpu
 rm qli-Client-1.9.*
 wget https://raw.githubusercontent.com/bagstah/qubic/main/appsettings.json
 sed -i "s/vastai1/$CONTAINER_ID/g" appsettings.json
-wget https://dl.qubic.li/downloads/qli-Client-1.9.7-Linux-x64.tar.gz
-tar zxvf qli-Client-1.9.7-Linux-x64.tar.gz qli-Client
+wget https://dl.qubic.li/downloads/qli-Client-2.1.3-Linux-x64.tar.gz
+tar zxvf qli-Client-2.1.3-Linux-x64.tar.gz qli-Client
 
 rm qli-runner*
 rm *.lock
